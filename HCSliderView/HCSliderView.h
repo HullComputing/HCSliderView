646a1d8720f608af48be8c13a9a6265efff64868
Aaron Hull <ahull@cobrain.com>
2014-06-06 08:49:16 -0400

    Initial Commit

---
+++ HCSliderView/HCSliderView.h
@@ -0,0 +1,19 @@
+//
+//  HCSliderView.h
+//  HCSliderView
+//
+//  Created by Aaron Hull on 6/6/14.
+//  Copyright (c) 2014 Hull Computing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HCSliderView.
+FOUNDATION_EXPORT double HCSliderViewVersionNumber;
+
+//! Project version string for HCSliderView.
+FOUNDATION_EXPORT const unsigned char HCSliderViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HCSliderView/PublicHeader.h>
+
+
---
+++ HCSliderViewTests/HCSliderViewTests.m
@@ -0,0 +1,39 @@
+//
+//  HCSliderViewTests.m
+//  HCSliderViewTests
+//
+//  Created by Aaron Hull on 6/6/14.
+//  Copyright (c) 2014 Hull Computing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HCSliderViewTests : XCTestCase
+
+@end
+
+@implementation HCSliderViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
